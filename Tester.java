6c333025df180eb94d1f7e11615045fae71affb6
Sadat40 <sahmed40@stuy.edu>
2023-06-01 15:32:38 -0400

    transfer further testing

--- Tester.java
+++ Tester.java
@@ -2,18 +2,23 @@ public class Tester{
   public static void main (String[] args){
     System.out.println("HELLO");
     Tube filler= new Tube();
-    Tube test= new Tube(7);
-    Tube test2= new Tube(2);
+    Tube test= new Tube(8);
+    Tube test2= new Tube(3);
     filler.fill(test);
-    System.out.println("HELLO");
-    System.out.println(test + " 7");
-    System.out.println(test2+ " 0");
-    filler.empty(test);
-    System.out.println(test + " 0"+ test.returnBalls());
+    System.out.println(test + " 8");
     System.out.println(test2+ " 0");
     test.transfer(test2);
-    System.out.println(test + " 0" + test.returnBalls());
-    System.out.println(test2+ " 0"+ test2.returnBalls());
+    System.out.println(test + " 5");
+    System.out.println(test2+ " 3");
+    test2.transfer(test);
+    System.out.println(test + " 8");
+    System.out.println(test2+ " 0");
+    filler.empty(test);
+    System.out.println(test + " 0");
+    System.out.println(test2+ " 0");
+    test.transfer(test2);
+    System.out.println(test + " 0");
+    System.out.println(test2+ " 0");
 
 
     //System.out.println(euclid(15,35));
